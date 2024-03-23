2e45e0183e3e15ddc8307377451ec8f5097854e1
Daniel Thompson <daniel.thompson@holbertonschool.com>
2024-03-23 03:09:32 -0500

    added more doc comments to symbolreader.h

--- symbolreader.h
+++ symbolreader.h
@@ -42,13 +42,30 @@ typedef void (*sym_action_t)(SymbolReader *reader, Elf64_Sym *symbol);
  */
 typedef int (*sym_filter_t)(SymbolReader *reader, Elf64_Sym *symbol);
 
+/**
+ * SymbolReader_Init - Initialize `SymbolReader` struct
+ * @reader: Pointer to `SymbolReader` struct
+ * @fpath: Path to ELF file
+ *
+ * Return: `0` on success, `-1` on failure
+ */
 int
 SymbolReader_Init(SymbolReader *reader, const char *fpath);
 
+/**
+ * SymbolReader_ProcessSymbols - Sequentially performs `action` on all symbols
+ * @reader: Pointer to `SymbolReader` structure
+ * @action: Pointer to symbol-processing function
+ * @filter: Pointer to optional filter function, symbol processed when true
+ */
 void
 SymbolReader_ProcessSymbols(SymbolReader *reader, sym_action_t action,
 	sym_filter_t filter);
 
+/**
+ * SymbolReader_Destroy - Releases resources held by `SymbolReader` struct
+ * @reader: Pointer to `SymbolReader` structure
+ */
 void
 SymbolReader_Destroy(SymbolReader *reader);
 
